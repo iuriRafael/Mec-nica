ccf5d46ee4174948e6f796a51527933ca9a5976a
Iuri <iuri-rdutra2@educar.rs.gov.br>
2023-12-08 15:29:02 -0300

    render

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,11 @@ require 'active_record'
 
 ActiveRecord::Base.establish_connection(
   adapter: 'mysql2',
-  host: 'localhost',
+  host: 'viaduct.proxy.rlwy.net',
   username: 'root',
-  password: '', # Senha do seu banco, se aplicável
-  database: 'Mecanico',
-  port: 3306
+  database: 'railway',
+  port: 57779
+  password: 'F61cgdEe-F54eccaFabgEGh24H-6dh15'
 )
 
 Dir["./app/models/*.rb"].each { |file| require file }
